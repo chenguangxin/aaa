86100e6b03c1ffed8eff631c580ceab4c90b4902
chenguangxin <2587804979@qq.com>
2019-03-29 10:32:40 +0800

    main

--- src/com/jbit/test/T.java
+++ src/com/jbit/test/T.java
@@ -2,6 +2,6 @@ package com.jbit.test;
 
 public class T {
     public static void main(String[] args) {
-        System.out.println("你好啊，小老弟！！！");
+        System.out.println("hello world！！！");
     }
 }
